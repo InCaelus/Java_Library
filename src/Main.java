960b54719f7a33c1e6e8eb5b3627fa7adf023479
Adelio <2024011862@office.cju.ac.kr>
2025-11-25 23:31:40 +0900

    2025.11.25
    - 긴급 복구 시도 1차

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-    System.out.println("도서관 프로그램 로딩중!");
-    System.out.println("도서관 프로그램 로딩 완료 !");
+        System.out.println("도서관 프로그램 로딩중!");
+        System.out.println("도서관 프로그램 로딩 완료 !");
 
-    Library library = new Library();
-
-    library.userInput();
-    library.start();
+        Library library = new Library();
     }
 }
